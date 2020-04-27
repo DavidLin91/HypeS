ec228cd89e8827fa5e77c5f58a1354784e9f48c7
David <lin.david985@gmail.com>
2020-04-27 10:12:14 -0400

    created passion project

---
+++ Hype S/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Hype S
+//
+//  Created by David Lin on 4/27/20.
+//  Copyright © 2020 David Lin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hype S/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Hype S
+//
+//  Created by David Lin on 4/27/20.
+//  Copyright © 2020 David Lin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
